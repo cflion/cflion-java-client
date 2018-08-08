2d997c4a0a082b86ebc8e3a27e63b8c343e0130f
ckanner <kanner.chen@gmail.com>
2018-08-08 15:48:29 +0800

    update

--- src/main/java/com/jcflion/App.java
+++ src/main/java/com/jcflion/App.java
@@ -1,5 +1,7 @@
 package com.jcflion;
 
+import com.jcflion.gray.GrayConfigManager;
+
 /**
  * @author kanner
  */
@@ -11,6 +13,8 @@ public class App {
             Thread.sleep(5000L);
             System.out.println("app1-test-file.cflion-key1=" + ConfigManager.getConfig("app1-test-file.cflion-key1"));
             System.out.println("app2-test-file.cflion-key2=" + ConfigManager.getConfig("app2-test-file.cflion-key2"));
+            System.out.println("app3-test-file.cflion-key3=" + ConfigManager.getConfig("app3-test-file.cflion-key3"));
+            System.out.println("account1 is allowd=" + GrayConfigManager.isAllowed("app3-test-file.cflion-key3", "account1"));
         }
     }
 }
