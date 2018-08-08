84b3b92534bfa79e3d38eb17ea15bb042790e445
ckanner <kanner.chen@gmail.com>
2018-08-08 14:48:30 +0800

    updatep

--- src/main/java/com/jcflion/App.java
+++ src/main/java/com/jcflion/App.java
@@ -7,7 +7,10 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
         new InitConfig("app1", "http://127.0.0.1:8080").init();
-        Thread.sleep(5000L);
-        System.out.println(ConfigManager.getConfig("app1-test-file.cflion-key1"));
+        while (true) {
+            Thread.sleep(5000L);
+            System.out.println("app1-test-file.cflion-key1=" + ConfigManager.getConfig("app1-test-file.cflion-key1"));
+            System.out.println("app2-test-file.cflion-key2=" + ConfigManager.getConfig("app2-test-file.cflion-key2"));
+        }
     }
 }
