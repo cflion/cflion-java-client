1fbad79e1194d9137bd010e52f33aea9ebde4964
ckanner <kanner.chen@gmail.com>
2018-08-09 10:24:32 +0800

    update app

--- src/main/java/com/jcflion/App.java
+++ src/main/java/com/jcflion/App.java
@@ -11,10 +11,13 @@ public class App {
         new InitConfig("app1", "http://127.0.0.1:8080").init();
         while (true) {
             Thread.sleep(5000L);
-            System.out.println("app1-test-file.cflion-key1=" + ConfigManager.getConfig("app1-test-file.cflion-key1"));
-            System.out.println("app2-test-file.cflion-key2=" + ConfigManager.getConfig("app2-test-file.cflion-key2"));
-            System.out.println("app3-test-file.cflion-key3=" + ConfigManager.getConfig("app3-test-file.cflion-key3"));
-            System.out.println("account1 is allowd=" + GrayConfigManager.isAllowed("app3-test-file.cflion-key3", "account1"));
+            System.out.println("cnf1.key1=" + ConfigManager.getConfig("cnf1.key1"));
+            System.out.println("cnf1.key2=" + ConfigManager.getConfig("cnf1.key2"));
+            System.out.println("account1 is allowed=" + GrayConfigManager.isAllowed("cnf1.switch", "account1"));
+            System.out.println("account2 is allowed=" + GrayConfigManager.isAllowed("cnf1.switch", "account2"));
+            System.out.println("account3 is allowed=" + GrayConfigManager.isAllowed("cnf1.switch", "account3"));
+            System.out.println("cnf2.key3=" + ConfigManager.getConfig("cnf2.key3"));
+            System.out.println("------------------------");
         }
     }
 }
